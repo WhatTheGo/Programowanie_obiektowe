6127b5ca281f454d85588619a66a282d3eaa28ac
WhatTheGo <147511154+WhatTheGo@users.noreply.github.com>
2024-10-04 14:48:31 +0200

    Create zad6.java

---
+++ zad6.java
@@ -0,0 +1,10 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class HelloWorld {
+    public static void main(String[] args) {
+        String txt = "Ala ma kota";
+        System.out.println(txt.toLowerCase());
+        System.out.println(txt.toUpperCase());
+    }
+}
