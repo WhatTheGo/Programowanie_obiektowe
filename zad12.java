f34f76ab689dadeaa2cb3208658690003b7e4262
WhatTheGo <147511154+WhatTheGo@users.noreply.github.com>
2024-10-16 18:11:03 +0200

    Delete zad12.java

--- zad12.java
+++
@@ -1,9 +0,0 @@
-// Zad 12
-
-class HelloWorld {
-    public static void main(String[] args) {
-        String myStr = "Dzisiaj jest (%d-%d-%d) godzina (%d:%d:%d)";
-        String result = String.format(myStr, 10, 12, 2024, 21, 12, 23);
-        System.out.println(result);
-    }
-}
