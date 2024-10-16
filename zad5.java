51ccf02b26d3755c7cdbb1ec7075935ab9a592d6
WhatTheGo <147511154+WhatTheGo@users.noreply.github.com>
2024-10-16 18:10:35 +0200

    Delete zad5.java

--- zad5.java
+++
@@ -1,9 +0,0 @@
-// Online Java Compiler
-// Use this editor to write, compile and run your Java code online
-
-class HelloWorld {
-    public static void main(String[] args) {
-        String txt = "Ala ma kota";
-        System.out.print(txt.replace("a", "e"));
-    }
-}
